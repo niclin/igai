7876319c2e391c69f225f192e3ed066eb12e9d73
niclin <bboyceo@hotmail.com>
2018-12-12 02:47:44 +0800

    Add broadcast

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -3,6 +3,7 @@ class MessageBroadcastJob < ApplicationJob
 
   def perform(message)
     ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel", message: render_message(message)
+    ActionCable.server.broadcast "notifications_#{message.chat_room.product.user.id}_channel", message: "New Message"
   end
 
   private

25576c45313178858defd743367ee00779e9064d
niclin <bboyceo@hotmail.com>
2018-12-17 23:31:37 +0800

    Add user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+  USERNAME_PATTERN = /\A[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*\z/
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 5 }, if: :name_changed?, on: :update
+
+  before_validation :create_user_name
 
   has_many :products
   has_many :sender_chat_rooms, class_name: "ChatRoom", foreign_key: "sender_id"
@@ -21,5 +27,23 @@ class User < ApplicationRecord
     Rails.cache.fetch("#{self.id}-unread-messages-count", expires_in: 1.minute) do
       Message.where(chat_room_id: chat_rooms.ids).by_recipient(self.id).unread.count
     end
+  end
+
+  def create_user_name
+    return if self.name.present?
+
+    email_user_name = self.email.split("@").first.tr(".", "_")
+
+    self.name = valid_user_name?(email_user_name) ? email_user_name : random_user_name
+  end
+
+  private
+
+  def random_user_name
+    "u#{Time.zone.now.to_i}"
+  end
+
+  def valid_user_name?(user_name)
+    USERNAME_PATTERN.match(user_name) && User.where("lower(name) = ?", user_name.downcase).blank?
   end
 end

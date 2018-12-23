c4035801ab78e0093fb594eeedb3942f47c975f8
niclin <bboyceo@hotmail.com>
2018-12-24 01:16:57 +0800

    Minor refactor

--- app/controllers/account/products_controller.rb
+++ app/controllers/account/products_controller.rb
@@ -31,7 +31,7 @@ class Account::ProductsController < Account::BaseController
 
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to account_products_path, notice: 'Product was successfully updated.' }
+        format.html { redirect_to account_products_path, notice: "#{@product.title} 更新成功！" }
         format.json { render json: { redirectURL: account_products_url }.to_json, status: :ok }
       else
         format.html { render :edit }

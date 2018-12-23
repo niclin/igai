f68f6bb2721a6c029a5dbaec642b2fbc00738d89
niclin <bboyceo@hotmail.com>
2018-12-24 04:28:13 +0800

    Add landing page og meta tag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.asset_host = 'https://igai.info'
 end

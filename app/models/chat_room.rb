73f5fa4cfafe35d47b74bb0b4360cf17bf4c6c4f
niclin <bboyceo@hotmail.com>
2018-12-25 23:16:19 +0800

    Add chat room dependent

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -3,7 +3,7 @@ class ChatRoom < ApplicationRecord
   belongs_to :sender , class_name: "User"
   belongs_to :receiver, class_name: "User"
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   def access?(user)
     [sender, receiver].include?(user)

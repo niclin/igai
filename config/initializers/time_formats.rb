c60fa75056c17c6cc8a436b6b433e58fbd4201b0
niclin <bboyceo@hotmail.com>
2018-12-25 23:26:38 +0800

    Set common time format

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,1 @@
+Time::DATE_FORMATS.merge!(default: "%Y-%m-%d %H:%M", ymd: "%Y-%m-%d", cndate: "%Y年%-m月%-d日", cndate_with_time: "%Y年%-m月%-d日 %H:%M", exact: "%Y-%m-%d %H:%M:%S", short_date_time: "%m-%d %H:%M")

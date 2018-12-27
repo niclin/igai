47b993c9cb5260ded41f68565146a105e5adcc13
niclin <bboyceo@hotmail.com>
2018-12-28 05:30:34 +0800

    Add production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,8 @@ Rails.application.configure do
 
   config.asset_host = 'https://igai.info'
 
+  config.action_mailer.default_url_options = { host: "https://igai.info" }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.mailgun.org',

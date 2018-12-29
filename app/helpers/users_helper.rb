16dc6672f627e694543f6ae6f031f7799f5f4140
niclin <bboyceo@hotmail.com>
2018-12-30 03:02:31 +0800

    Add line friend button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def render_user_line_button(user)
+    if user.line_url.present?
+      link_to(user.line_url) do
+        image_tag("https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png", alt: "加入好友", height: 36, border: 0)
+      end
+    end
+  end
 end

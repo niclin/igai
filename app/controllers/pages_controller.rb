cbc329a323eed2936b6d913ff7edbb947dd4816c
niclin <bboyceo@hotmail.com>
2018-12-31 03:33:45 +0800

    Add devise seo title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def about
     set_meta_tags title: "關於本站"
   end
+
+  def privacy
+    set_meta_tags title: "隱私權政策"
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,7 @@ class UsersController < ApplicationController
     @buy_products = products.where(product_type: "buy")
     @sell_products = products.where(product_type: "sell")
     @exchange_products = products.where(product_type: "exchange")
+
+    set_meta_tags title: "#{user.name} 的個人主頁"
   end
 end

8bbd3fbec8c1cc6657b881078efedaf6b000b4f4
niclin <bboyceo@hotmail.com>
2018-12-31 01:26:05 +0800

    Set default seo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@ class HomeController < ApplicationController
   layout "home"
 
   def index
-    set_meta_tags title: "機車二手精品交流買賣網",
-                  description: "94愛改，交流買賣精品不再麻煩",
-                  og: {
-                    image: ActionController::Base.helpers.asset_path('showcase-3.jpg')
-                  }
+    set_meta_tags title: "機車二手精品交流買賣網"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
-
+  def default_meta_tags
+    {
+      site: "iGai 愛改",
+      description: '最好用的機車改裝精品二手交流買賣網，交流買賣精品不再麻煩。',
+      keywords:    '勁戰, 雷霆, 58.5, 機車改裝, 二手機車精品, MSP大熱狗, 鍛造框, 排氣管, MDF2, DDS, 燒鈦, 合法館, 魚眼, 卡夢, 後扶手, 烤漆, 碳纖維, 鑄造框, 避震器, 前叉, 倒叉, BT601, 汽缸, KOSO, NCY, 新雅, TTMRC',
+      og: {
+        image: ActionController::Base.helpers.asset_path('showcase-3.jpg')
+      },
+      fb: {
+        app_id: '2228012483910850'
+      }
+    }
+  end
 end
